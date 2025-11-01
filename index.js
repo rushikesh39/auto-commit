d063cd2708ad3d2f551f2b138b4025ee23db53a9
rushi <rushikeshthange2001@gmail.com>
2025-11-01 13:51:19 +0530

    second commit

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-import autoCommitRouter from "./autoCommit";
+import express from "express";
+import autoCommitRouter from "./autoCommit.js";
 
+const app = express();
 app.use("/auto-commit", autoCommitRouter);
+
+app.listen(8000, () => console.log("✅ Server running on port 8000"));
